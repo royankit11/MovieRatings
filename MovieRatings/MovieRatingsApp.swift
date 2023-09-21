e380d3219e904362eeae93cf581881e1e3ea3063
royankit11 <royankit11@gmail.com>
2023-09-21 18:17:48 -0400

    Initial Commit

---
+++ MovieRatings/MovieRatingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRatingsApp.swift
+//  MovieRatings
+//
+//  Created by Rik Roy on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieRatingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
